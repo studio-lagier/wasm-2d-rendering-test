6a37fa322c7a57ee902be5aba719b332b43b1992
Tom Lagier <tom@gliffy.com>
2021-05-15 19:08:42 -0700

    JS renderer

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use wasm_bindgen::prelude::*;
 use tiny_skia::*;
 
 extern crate web_sys;
+
 // A macro to provide `println!(..)`-style syntax for `console.log` logging.
 macro_rules! log {
     ( $( $t:tt )* ) => {
@@ -27,28 +28,15 @@ pub enum Cell {
 }
 
 #[wasm_bindgen]
-#[repr(C)]
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-// r, g, b, a
-pub struct Pixel (u8, u8, u8, u8);
-
-#[wasm_bindgen]
 pub struct Universe {
     width: i32,
     height: i32,
-    pixel_width: u32,
-    pixel_height: u32,
-    cells: Vec<Cell>,
-    pixmap: Pixmap
+    cells: Vec<Cell>
 }
 
 impl Universe {
     fn get_cell_index(&self, row: i32, col: i32) -> usize {
         (row * self.width + col) as usize
-    }
-
-    fn get_pixel_index(&self, row: i32, col: i32) -> usize {
-        ((row * self.width + col) * 4) as usize
     }
 
     fn live_neighbor_count(&self, row: i32, col: i32) -> u8 {
@@ -74,7 +62,6 @@ impl Universe {
 impl Universe {
     pub fn tick(&mut self) {
         self.build_board();
-        self.render_board();
     }
 
     fn build_board(&mut self) {
@@ -101,60 +88,7 @@ impl Universe {
         self.cells = next;
     }
 
-    // We map cells to pixels - this would get replaced with a proper
-    fn render_board(&mut self) {
-        // Draw our grid
-        let cell_width = (self.pixel_width - 2) as f32 / self.width as f32;
-        let cell_height = (self.pixel_height - 2) as f32 / self.height as f32;
-
-        let mut grid_color = Paint::default();
-        grid_color.set_color_rgba8(0xff, 0xdd, 0xdd, 0xdd);
-        let mut alive_color = Paint::default();
-        alive_color.set_color_rgba8(0, 0, 0, 0xff);
-        let mut dead_color = Paint::default();
-        dead_color.set_color_rgba8(0xff, 0xff, 0xff, 0xff);
-
-        // Draw our alive squares
-        // let mut alive_pb = PathBuilder::new();
-        // let mut dead_pb = PathBuilder::new();
-
-        for row in 0..self.width {
-            for col in 0..self.height {
-                let idx = self.get_cell_index(row, col);
-                let cell = self.cells[idx];
-
-                let x = row as f32 * cell_height + 1.;
-                let y = col as f32 * cell_width + 1.;
-
-                // let loop_pb = match cell {
-                //     Cell::Alive => &mut alive_pb,
-                //     Cell::Dead => &mut dead_pb
-                // };
-
-                // loop_pb.move_to(x, y);
-                // loop_pb.line_to(x + cell_width, y);
-                // loop_pb.line_to(x + cell_width, y + cell_height);
-                // loop_pb.line_to(x, y + cell_height);
-                // loop_pb.close();
-
-                let color = match cell {
-                    Cell::Alive => &alive_color,
-                    Cell::Dead => &dead_color
-                };
-
-                let rect = Rect::from_xywh(x, y, cell_width, cell_height).unwrap();
-                self.pixmap.fill_rect(rect, &color, Transform::identity(), None);
-            }
-        }
-
-        // let alive_path = alive_pb.finish().unwrap();
-        // let dead_path = dead_pb.finish().unwrap();
-
-        // self.pixmap.fill_path(&alive_path, &alive_color, FillRule::Winding, Transform::identity(), None);
-        // self.pixmap.fill_path(&dead_path, &dead_color, FillRule::Winding, Transform::identity(), None);
-    }
-
-    pub fn new(pixel_width: u32, pixel_height: u32) -> Universe {
+    pub fn new() -> Universe {
         let width = 64;
         let height = 64;
 
@@ -168,15 +102,10 @@ impl Universe {
             })
             .collect();
 
-        let pixmap = Pixmap::new(pixel_width, pixel_height).unwrap();
-
         Universe {
             width,
             height,
-            pixel_width,
-            pixel_height,
             cells,
-            pixmap
         }
     }
 
@@ -190,9 +119,5 @@ impl Universe {
 
     pub fn cells(&self) -> *const Cell {
         self.cells.as_ptr()
-    }
-
-    pub fn image_data(&self) -> *const u8 {
-        self.pixmap.data().as_ptr()
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,7 +1,7 @@
 import { Universe, Cell } from "wasm-game-of-life";
 import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
 
-const CELL_SIZE = 5; // px
+const CELL_SIZE = 11; // px
 const GRID_COLOR = "#CCCCCC";
 const DEAD_COLOR = "#FFFFFF";
 const ALIVE_COLOR = "#000000";
@@ -10,36 +10,84 @@ const canvas = document.getElementById('game-of-life-canvas');
 const fpsCounter = document.getElementById('fps-count');
 let currentTime = Date.now();
 
-canvas.height = 768;
-canvas.width = 768;
-
 const universe = Universe.new(canvas.width, canvas.height);
-// const width = universe.width();
-// const height = universe.height();
+
+const width = universe.width();
+const height = universe.height();
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
 
 const ctx = canvas.getContext('2d');
-
-// Number of pixels in grid
-const imageDataSize = canvas.width * canvas.height * 4;
-const imageData = new ImageData(canvas.width, canvas.height);
 
 // JS-driven loop. Can we make it Rust-driven?
 // Answer: Unlikely, unless we want to use RAF always since it seems that Rust
 // depends on the platform for a timer
 const renderLoop = () => {
   universe.tick();
-  const pixels = new Uint8Array(memory.buffer, universe.image_data(), imageDataSize);
-  imageData.data.set(pixels);
-  ctx.putImageData(imageData, 0, 0);
+
+  drawGrid();
+  drawCells();
+
 
   const now = Date.now();
   const timeSinceLastFrame = now - currentTime;
   currentTime = now;
 
   const FPS = 1000 / timeSinceLastFrame;
-  fpsCounter.textContent = FPS;
+  fpsCounter.textContent = Math.round(FPS);
 
   requestAnimationFrame(renderLoop);
 };
 
+const getIndex = (row, column) => {
+  return row * width + column;
+};
+
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  // Vertical lines.
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  // Horizontal lines.
+  for (let j = 0; j <= height; j++) {
+    ctx.moveTo(0,                           j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+};
+
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = cells[idx] === Cell.Dead
+        ? DEAD_COLOR
+        : ALIVE_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  ctx.stroke();
+};
+
+drawGrid();
+drawCells();
 requestAnimationFrame(renderLoop);
