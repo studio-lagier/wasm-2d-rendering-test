98210e201471bb2c4fab0c5408c0b417bfb8ce26
Tom Lagier <tom@gliffy.com>
2021-05-16 23:02:37 -0700

    Working pathfinder implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,150 +1,242 @@
-
-use wasm_bindgen::prelude::*;
-
 mod utils;
-
-use euclid::default::Size2D;
-use pathfinder_canvas::{Canvas, CanvasFontContext, Path2D};
-use pathfinder_color::ColorF;
+use pathfinder_canvas::{Canvas, CanvasFontContext, Path2D, FillStyle };
+use pathfinder_color::{ColorF, ColorU};
+use pathfinder_content::fill::{FillRule};
 use pathfinder_geometry::rect::RectF;
-use pathfinder_geometry::vector::{vec2f, vec2i};
-use pathfinder_gl::{GLDevice, GLVersion};
-use pathfinder_renderer::concurrent::rayon::RayonExecutor;
-use pathfinder_renderer::concurrent::scene_proxy::SceneProxy;
-use pathfinder_renderer::gpu::options::{DestFramebuffer, RendererOptions};
+use pathfinder_geometry::vector::{vec2f, vec2i, Vector2I};
+use pathfinder_renderer::concurrent::executor::SequentialExecutor;
+use pathfinder_renderer::gpu::options::{DestFramebuffer, RendererMode, RendererOptions};
 use pathfinder_renderer::gpu::renderer::Renderer;
 use pathfinder_renderer::options::BuildOptions;
 use pathfinder_resources::embedded::EmbeddedResourceLoader;
-use surfman::{Connection, ContextAttributeFlags, ContextAttributes, GLVersion as SurfmanGLVersion};
-use surfman::{SurfaceAccess, SurfaceType};
-use winit::dpi::LogicalSize;
-use winit::{
-    event::{ Event, WindowEvent },
-    event_loop::{ ControlFlow, EventLoop },
-    window::WindowBuilder
-};
+use pathfinder_webgl::WebGlDevice;
+use wasm_bindgen::JsCast;
+use web_sys::{HtmlCanvasElement, WebGl2RenderingContext};
+use wasm_bindgen::prelude::*;
+
+extern crate web_sys;
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn main() {
-    // Open a window.
-    let mut event_loop = EventLoop::new();
-    let window_size = Size2D::new(640, 480);
-    let logical_size = LogicalSize::new(window_size.width as f64, window_size.height as f64);
-    let window = WindowBuilder::new().with_title("Minimal example")
-                                     .build(&event_loop)
-                                     .unwrap();
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1
+}
 
-    // Create a `surfman` device. On a multi-GPU system, we'll request the low-power integrated
-    // GPU.
-    let connection = Connection::from_winit_window(&window).unwrap();
-    let native_widget = connection.create_native_widget_from_winit_window(&window).unwrap();
-    let adapter = connection.create_low_power_adapter().unwrap();
-    let mut device = connection.create_device(&adapter).unwrap();
+#[wasm_bindgen]
+pub struct Universe {
+    width: i32,
+    height: i32,
+    pixel_width: u32,
+    pixel_height: u32,
+    cells: Vec<Cell>,
+    renderer: Renderer<WebGlDevice>,
+    framebuffer_size: Vector2I
+}
 
-    // Request an OpenGL 3.x context. Pathfinder requires this.
-    let context_attributes = ContextAttributes {
-        version: SurfmanGLVersion::new(3, 0),
-        flags: ContextAttributeFlags::ALPHA,
-    };
-    let context_descriptor = device.create_context_descriptor(&context_attributes).unwrap();
+impl Universe {
+    fn get_cell_index(&self, row: i32, col: i32) -> usize {
+        (row * self.width + col) as usize
+    }
 
-    // Make the OpenGL context via `surfman`, and load OpenGL functions.
-    let surface_type = SurfaceType::Widget { native_widget };
+    fn get_pixel_index(&self, row: i32, col: i32) -> usize {
+        ((row * self.width + col) * 4) as usize
+    }
 
-    // IDK if we need to set `share_with` to something
-    let mut context = device.create_context(&context_descriptor, None).unwrap();
-    let surface = device.create_surface(&context, SurfaceAccess::GPUOnly, surface_type)
-                        .unwrap();
-    device.bind_surface_to_context(&mut context, surface).unwrap();
-    device.make_context_current(&context).unwrap();
-    gl::load_with(|symbol_name| device.get_proc_address(&context, symbol_name));
+    fn live_neighbor_count(&self, row: i32, col: i32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
 
-    // Get the real size of the window, taking HiDPI into account.
-    let hidpi_factor = window.current_monitor().unwrap().scale_factor();
-    let physical_size: winit::dpi::PhysicalSize<u32> = logical_size.to_physical(hidpi_factor);
-    let framebuffer_size = vec2i(physical_size.width as i32, physical_size.height as i32);
-
-    // Create a Pathfinder GL device.
-    let default_framebuffer = device.context_surface_info(&context)
-                                    .unwrap()
-                                    .unwrap()
-                                    .framebuffer_object;
-    let pathfinder_device = GLDevice::new(GLVersion::GL3, default_framebuffer);
-
-    // Create a Pathfinder renderer.
-    let options = RendererOptions {
-        background_color: Some(ColorF::white()),
-    };
-    let resource_loader = EmbeddedResourceLoader::new();
-    let dest_framebuffer = DestFramebuffer::default();
-    let mut renderer = Renderer::new(pathfinder_device, &resource_loader, dest_framebuffer, options);
-
-    let font_context = CanvasFontContext::from_system_source();
-    let mut is_first_render = true;
-    // Wait for a keypress.
-    event_loop.run(move |event, _, control_flow| {
-        let mut should_render = is_first_render;
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => {
-                println!("The close button was pressed; stopping");
-                *control_flow = ControlFlow::Exit
-            },
-            Event::MainEventsCleared => {
-                // Application update code.
-
-                // Queue a RedrawRequested event.
-                //
-                // You only need to call this if you've determined that you need to redraw, in
-                // applications which do not always need to. Applications that redraw continuously
-                // can just render here instead.
-                window.request_redraw();
-            },
-            Event::RedrawRequested(_) => {
-                // Redraw the application.
-                should_render = true;
-            },
-            _ => ()
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (col + delta_col) % self.width;
+                let idx = self.get_cell_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
         }
 
-        if should_render {
-            // Make a canvas. We're going to draw a house.
-            let mut canvas = Canvas::new(framebuffer_size.to_f32()).get_context_2d(font_context.clone());
+        count
+    }
+}
 
-            // Set line width.
-            canvas.set_line_width(10.0);
+#[wasm_bindgen]
+impl Universe {
+    pub fn tick(&mut self) {
+        self.build_board();
+        self.render_board();
+    }
 
-            // Draw walls.
-            canvas.stroke_rect(RectF::new(vec2f(75.0, 140.0), vec2f(150.0, 110.0)));
+    fn build_board(&mut self) {
+        let mut next = self.cells.clone();
 
-            // Draw door.
-            canvas.fill_rect(RectF::new(vec2f(130.0, 190.0), vec2f(40.0, 60.0)));
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_cell_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
 
-            // Draw roof.
-            let mut path = Path2D::new();
-            path.move_to(vec2f(50.0, 140.0));
-            path.line_to(vec2f(150.0, 60.0));
-            path.line_to(vec2f(250.0, 140.0));
-            path.close_path();
-            canvas.stroke_path(path);
+                let next_cell = match (cell, live_neighbors) {
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (Cell::Dead, 3) => Cell::Alive,
+                    (otherwise, _) => otherwise,
+                };
 
-            // Render the canvas to screen.
-            let scene = SceneProxy::from_scene(canvas.into_canvas().into_scene(),
-                                                   RayonExecutor);
-            scene.build_and_render(&mut renderer, BuildOptions::default());
-
-            // Present the surface.
-            let mut surface = device.unbind_surface_from_context(&mut context).unwrap().unwrap();
-            device.present_surface(&mut context, &mut surface).unwrap();
-            device.bind_surface_to_context(&mut context, surface).unwrap();
+                next[idx] = next_cell;
+            }
         }
 
-        is_first_render = false;
-        *control_flow = ControlFlow::Poll
-    });
+        self.cells = next;
+    }
 
-    // Clean up.
-    drop(device.destroy_context(&mut context));
+    fn render_board(&mut self) {
+        let font_context = CanvasFontContext::from_system_source();
+        let mut canvas = Canvas::new(self.framebuffer_size.to_f32()).get_context_2d(font_context);
+
+        let cell_width = (self.pixel_width as f32 - 2.) / self.width as f32;
+        let cell_height = (self.pixel_height as f32 - 2.) / self.height as f32;
+
+        let mut alive = Path2D::new();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_cell_index(row, col);
+                let cell = self.cells[idx];
+
+                let x0 = col as f32 * cell_width;
+                let y0 = row as f32 * cell_height;
+
+                let rect = RectF::new(vec2f(x0, y0), vec2f(cell_width, cell_height));
+
+                match cell {
+                    // This is kinda slow, not sure how we can speed it up
+                    Cell::Alive => alive.rect(rect),
+                    _ => ()
+                }
+            }
+        }
+
+        let alive_color = ColorU {
+            r: 0,
+            g: 0,
+            b: 0,
+            a: 0xff
+        };
+
+        canvas.set_fill_style(FillStyle::Color(alive_color));
+        canvas.fill_path(alive, FillRule::Winding);
+
+        let mut grid = Path2D::new();
+
+        for row in 0..=self.height {
+            let y = (row as f32 * cell_height) + 1.;
+
+            grid.move_to(vec2f(0., y));
+            grid.line_to(vec2f(self.pixel_width as f32, y));
+        }
+
+        for col in 0..=self.width {
+            let x = (col as f32 * cell_width) + 1.;
+
+            grid.move_to(vec2f(x, 0.));
+            grid.line_to(vec2f(x, self.pixel_height as f32));
+        }
+
+        let grid_color = ColorU {
+            r: 0xdd,
+            g: 0xdd,
+            b: 0xdd,
+            a: 0xff
+        };
+
+        canvas.set_stroke_style(FillStyle::Color(grid_color));
+        canvas.stroke_path(grid);
+
+        // Render the canvas to screen.
+        let mut scene = canvas.into_canvas().into_scene();
+        scene.build_and_render(&mut self.renderer, BuildOptions::default(), SequentialExecutor);
+    }
+
+    pub fn new(pixel_width: u32, pixel_height: u32, canvas_id: &str ) -> Universe {
+        utils::set_panic_hook();
+
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+            let document = web_sys::window().unwrap().document().unwrap();
+            let canvas = document.get_element_by_id(canvas_id).unwrap();
+            let canvas: HtmlCanvasElement = canvas.dyn_into::<HtmlCanvasElement>().unwrap();
+
+            let context = canvas.get_context("webgl2")
+                                .unwrap()
+                                .unwrap()
+                                .dyn_into::<WebGl2RenderingContext>()
+                                .unwrap();
+
+            // Get the real size of the window, taking HiDPI into account.
+            let framebuffer_size = vec2i(canvas.width() as i32, canvas.height() as i32);
+
+            // Create a Pathfinder GL device.
+            let pathfinder_device = WebGlDevice::new(context);
+
+            // Create a Pathfinder renderer.
+            let mode = RendererMode::default_for_device(&pathfinder_device);
+            let options = RendererOptions {
+                dest: DestFramebuffer::full_window(framebuffer_size),
+                background_color: Some(ColorF::white()),
+                ..RendererOptions::default()
+            };
+            let resource_loader = EmbeddedResourceLoader::new();
+            let renderer = Renderer::new(pathfinder_device, &resource_loader, mode, options);
+
+        Universe {
+            width,
+            height,
+            pixel_width,
+            pixel_height,
+            cells,
+            renderer,
+            framebuffer_size,
+        }
+    }
+
+    pub fn width(&self) -> i32 {
+        self.width
+    }
+
+    pub fn height(&self) -> i32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,27 @@
-import { main } from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-main();
+const canvas = document.getElementById('game-of-life-canvas');
+canvas.width = 768;
+canvas.height = 768;
+const fpsCounter = document.getElementById('fps-count');
+let currentTime = Date.now();
+
+const universe = Universe.new(768, 768, "game-of-life-canvas");
+
+// JS-driven loop. Can we make it Rust-driven?
+// Answer: Unlikely, unless we want to use RAF always since it seems that Rust
+// depends on the platform for a timer
+const renderLoop = () => {
+  universe.tick();
+
+  const now = Date.now();
+  const timeSinceLastFrame = now - currentTime;
+  currentTime = now;
+
+  const FPS = 1000 / timeSinceLastFrame;
+  fpsCounter.textContent = Math.round(FPS);
+
+  setImmediate(renderLoop);
+};
+
+renderLoop();
