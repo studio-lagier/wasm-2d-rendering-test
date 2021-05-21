e7a10640be168c807b54d32215dd7bd133ee6842
Tom Lagier <tom@gliffy.com>
2021-05-21 11:19:44 -0700

    Better FPS counter

--- www/index.js
+++ www/index.js
@@ -4,7 +4,8 @@ const canvas = document.getElementById('game-of-life-canvas');
 canvas.width = 768;
 canvas.height = 768;
 const fpsCounter = document.getElementById('fps-count');
-let currentTime = Date.now();
+let currentTime = performance.now();
+const lastHundredFrames = [];
 
 const universe = Universe.new(768, 768, "game-of-life-canvas");
 
@@ -14,12 +15,19 @@ const universe = Universe.new(768, 768, "game-of-life-canvas");
 const renderLoop = () => {
   universe.tick();
 
-  const now = Date.now();
+  const now = performance.now();
   const timeSinceLastFrame = now - currentTime;
   currentTime = now;
 
   const FPS = 1000 / timeSinceLastFrame;
-  fpsCounter.textContent = Math.round(FPS);
+  if (lastHundredFrames.length > 100) {
+    lastHundredFrames.unshift();
+  }
+
+  lastHundredFrames.push(FPS);
+
+  const averageFPS = lastHundredFrames.reduce((a, b) => a + b) / lastHundredFrames.length;
+  fpsCounter.textContent = Math.round(averageFPS);
 
   setImmediate(renderLoop);
 };
